f9f8457ebc1bdbe844a165dfece32729e04c4490
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-05-03 01:27:50 +0300

    Adding customer, itemCollection classes

---
+++ src/CustomerDetails/Customer.java
@@ -0,0 +1,25 @@
+package CustomerDetails;
+import  OrderDetails.Order;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private Account account;
+    private int id;
+    private ArrayList<Order> orders;
+    public void login(){
+
+    }
+    public void register(){
+
+    }
+    public void addToCart(){
+
+    }
+    public void checkout(){
+
+    }
+    public void pay(){
+
+    }
+}
