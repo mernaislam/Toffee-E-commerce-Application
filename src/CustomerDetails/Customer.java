c68a653c9a843ef287081678333e2d26c86cf700
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2023-05-05 20:02:53 +0300

    edits

--- src/CustomerDetails/Customer.java
+++ src/CustomerDetails/Customer.java
@@ -2,35 +2,104 @@ package CustomerDetails;
 import  OrderDetails.Order;
 import Items.Items;
 import ShoppingCart.Cart;
+import FileIO.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Customer {
     private Account account;
     private int id;
     private Cart cart;
-    private ArrayList<Order> orders;
+    private ArrayList<Order> orders = new ArrayList<Order>();
     public void login(String username, String password){
-        // search in a file
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("Toffee-E-commerce-Application/CustomerDetails.txt"));
+            String line;
+            while((line = reader.readLine()) != null){
+                if(line.equals(username)){
+                    line = reader.readLine();
+                    if(line.equals(password)){
+                        System.out.println("Successfully Logged in");
+                    } else {
+                        int cnt = 3;
+                        while(cnt != 0){
+                            System.out.print("Incorrect Password, please try again: ");
+                            Scanner s = new Scanner(System.in);
+                            password = s.nextLine();
+                            if(line.equals(password)){
+                                System.out.println("Successfully Logged in");
+                                break;
+                            }
+                            cnt--;
+                        }
+                        if(cnt == 0){
+                            System.out.println("Failed to log in");
+                            return;
+                        }
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        mainMenu();
+    }
+
+    public void mainMenu(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome to Toffee Application");
+        System.out.println("1. View Catalog (All Items). \n2. View Catalog by Categories. \n3. checkout. \n4. Exit");
+        System.out.println("Please enter your choice: ");
+        int choice = s.nextInt();
     }
     public void register(){
         Scanner s = new Scanner(System.in);
         System.out.print("Please enter a username: ");
         String username = s.nextLine();
+
+        // verify username does not exists to prevent duplicates
+        ReadingFromFile read = new ReadingFromFile();
+        while(read.isEmailExist("CustomerDetails", username)){
+            System.out.print("Username already exists, please enter another username: ");
+            username = s.nextLine();
+        }
+
         System.out.print("Please enter an email: ");
         String email = s.nextLine();
+
+        // validate email
+        while(!isValidEmail(email)){
+            System.out.print("Please enter a valid email: ");
+            email = s.nextLine();
+        }
+
         System.out.print("Please enter a password: ");
         String pass1 = s.nextLine();
+
+        // regex strong password
+        while(!isValidPassword(pass1)){
+            System.out.print("Please enter a strong password: ");
+            pass1 = s.nextLine();
+        }
         System.out.print("Please re-enter the password: ");
         String pass2 = s.nextLine();
-        while(pass1 != pass2){
+
+        // validate password in re-entering
+        while(!pass1.equals(pass2)){
             System.out.println("Passwords does not match, please re-enter the passwords:");
-            System.out.print("Please enter a password: ");
-            pass1 = s.nextLine();
             System.out.print("Please re-enter the password: ");
             pass2 = s.nextLine();
         }
+
+        // Address Filling
         System.out.println("Great, now we need to fill your address!");
         System.out.print("Governorate: ");
         String gov = s.nextLine();
@@ -44,18 +113,41 @@ public class Customer {
         int floorNo = s.nextInt();
         System.out.print("Flat Number: ");
         int flatNo = s.nextInt();
+
         Address add = new Address(gov, street, district, buildingNo, floorNo, flatNo);
         Account acc = new Account(username, email, pass1, add);
         account = acc;
-        // add username and password to a file
+
+        // Writing data to file
+        WritingToFile writeUsername = new WritingToFile("CustomerDetails", username);
+        WritingToFile writePassword = new WritingToFile("CustomerDetails", pass1);
     }
+
     public void addToCart(Items item){
         cart.addItem(item);
     }
+
     public void checkout(){
 
     }
+
     public void pay(){
 
+    }
+
+    public static boolean isValidEmail(String email)
+    {
+        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean isValidPassword(String password)
+    {
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{8,20}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 }
--- src/FileIO/ReadingFromFile.java
+++ src/FileIO/ReadingFromFile.java
@@ -7,15 +7,33 @@ import java.io.IOException;
 public class ReadingFromFile {
     BufferedReader writer;
 
-    public ReadingFromFile(){
+    public void display(String fileName){
         try {
-            writer = new BufferedReader (new FileReader("Toffee-E-commerce-Application/userData.txt"));
+            writer = new BufferedReader (new FileReader("Toffee-E-commerce-Application/" + fileName + ".txt"));
             String line;
-            while((line = writer.readLine()) != null)
+            while((line = writer.readLine()) != null){
                 System.out.println(line);
+            }
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+    public boolean isEmailExist(String fileName, String toSearch){
+        try {
+            writer = new BufferedReader (new FileReader("Toffee-E-commerce-Application/" + fileName + ".txt"));
+            String line;
+            while((line = writer.readLine()) != null){
+                if(line.equals(toSearch)){
+                   return true;
+                }
+                line = writer.readLine();
+            }
+            writer.close();
+            return false;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
