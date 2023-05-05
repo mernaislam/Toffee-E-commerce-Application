48add24655fcd69dfea8f6af7ce9b7b38528a598
mariaehab01 <maria.ehab2004@gmail.com>
2023-05-05 18:23:57 +0300

    updating cart /orderDetails

--- src/OrderDetails/Order.java
+++ src/OrderDetails/Order.java
@@ -1,4 +1,30 @@
 package OrderDetails;
+import CustomerDetails.Address;
+import ShoppingCart.Cart;
+import java.util.Date;
 
 public class Order {
+    private String id;
+    private Date date;
+    private Double totalPrice;
+    private Address shippingAddress;
+    private Cart cart;
+    //Payment paymentMethod;
+    private OrderStatus status;
+
+    public Order(String id,Cart cart){
+        this.id = id;
+        this.cart = cart;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
